3c61ad3d26a41f9096f055934664c8e9e05e9369
IGOR <i.chesnishyy@gmail.com>
2016-11-11 14:16:26 +0200

    html/css - done

---
+++ script/script.js
@@ -0,0 +1,10 @@
+/*
+(function () {
+    var button = document.createElement('button');
+    button.classList.add('button');
+    button.innerHTML = 'Button';
+    var wrapper = document.getElementById('wrapper');
+
+    console.log(wrapper);
+    wrapper.appendChild(button);
+})();*/
